7634b5f51d1e46f38d2028647aa51c7cbe421a40
Rodrigo Guimarães <guimaraes_rodrigues@yahoo.com>
2021-05-05 15:22:55 -0300

    add tests for home component

--- src/app/modules/home/home.component.spec.ts
+++ src/app/modules/home/home.component.spec.ts
@@ -4,20 +4,43 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { RouterTestingModule } from '@angular/router/testing';
+import { of, throwError } from 'rxjs';
 
+import { JokeService } from 'src/app/shared/services/jokes.service';
 import { HomeComponent } from './home.component';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let jokeServiceStub: jasmine.SpyObj<JokeService>;
+
+  const queryMock = {
+    total: 2, 
+    result: [{id: 'abc', value: 'blabla'}]
+  };
+
+  const errorMock = {error: 'error'}
 
   beforeEach(async () => {
+    jokeServiceStub = jasmine.createSpyObj('JokeService', [
+      'getFactsWithQuery'
+    ]);
+
+    jokeServiceStub.getFactsWithQuery.and.returnValue(of());
+
     await TestBed.configureTestingModule({
       imports: [
         HttpClientTestingModule,
         RouterTestingModule,
         MatSnackBarModule,
-        FormsModule
+        FormsModule,
+        RouterTestingModule
+      ],
+      providers: [
+        {
+          provide: JokeService,
+          useValue: jokeServiceStub
+        }
       ],
       declarations: [ HomeComponent ],
       schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
@@ -28,10 +51,45 @@ describe('HomeComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
+    component.searchQuery = 'blabla';
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call #navigateToResults when get facts returns with data', () => {
+    const spyOnNavigateToResults = spyOn(component as any, 'navigateToResults');
+    
+    jokeServiceStub.getFactsWithQuery.and.returnValue(of(queryMock));
+    component.searchJokes();
+
+    expect(spyOnNavigateToResults).toHaveBeenCalledWith(queryMock);
+  });
+
+  it('should call #displayRequestError when get facts returns with error', () => {
+    const spyOnDisplayRequestError = spyOn(component as any, 'displayRequestError');
+    
+    jokeServiceStub.getFactsWithQuery.and.returnValue(throwError(errorMock));
+    component.searchJokes();
+
+    expect(spyOnDisplayRequestError).toHaveBeenCalledWith(errorMock);
+  });
+
+  it('should navigate to /results when #navigateToResults is called', () => {
+    const spyOnNavigate = spyOn(component['router'], 'navigate');
+
+    component['navigateToResults'](queryMock);
+
+    expect(spyOnNavigate).toHaveBeenCalled();
+  });
+
+  it('should call snackbar open when #displayRequestError is called', () => {
+    const spyOndDisplayRequestError = spyOn(component['_snackBar'], 'open');
+
+    component['displayRequestError'](errorMock);
+
+    expect(spyOndDisplayRequestError).toHaveBeenCalled();
   });
 });
